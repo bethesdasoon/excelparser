7f4390fbd21381ded6149958bfd20d6fa14c9c40
sayem.ahmed <sayem.ahmed@impulsebdltd.com>
2013-06-19 21:06:08 +0600

    Initial project skeleton

---
+++ src/main/java/team/lizards/ncpdp/datafile/App.java
@@ -0,0 +1,13 @@
+package team.lizards.ncpdp.datafile;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
