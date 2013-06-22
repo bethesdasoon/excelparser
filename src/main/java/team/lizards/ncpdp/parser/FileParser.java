ab83eda997ce3ebc54a1712f6d1ab20d2c7bd3d9
sayembd <sayem64@gmail.com>
2013-06-22 23:06:46 +0600

    Added methods.

--- src/main/java/team/lizards/ncpdp/parser/FileParser.java
+++ src/main/java/team/lizards/ncpdp/parser/FileParser.java
@@ -1,22 +1,34 @@
 package team.lizards.ncpdp.parser;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
 
 public abstract class FileParser {
-  @SuppressWarnings("unused")
   private File file;
-  
+
   public FileParser() {
-    this.file = null;
+    setFile(null);
   }
-  
+
   public FileParser(File file) {
     setFile(file);
   }
-  
+
   public void setFile(File file) {
     this.file = file;
   }
-  
-  public abstract void parse();
+
+  public File getFile() throws FileNotFoundException {
+    if (file == null) {
+      throw new FileNotFoundException("Missing file. Pass the file " +
+          "using setFile method before parsing.");
+    }
+    
+    return file;
+  }
+
+  public abstract Map<Integer, String> getFieldNames() throws Exception;
+
+  public abstract void getFields() throws Exception;
 }
